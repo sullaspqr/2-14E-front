1d01f820245624d76db060b12d1f39d47b386a63
Németh Bence <sulla.spqr78@gmail.com>
2022-09-08 19:16:22 +0200

    UPDATE

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var a =2;
+var b =3;
+switch (a){
+    case 1:
+        a=a+1;
+        console.log(a);
+        break;
+    case 2:
+        a=a-1;
+        console.log(a);
+        break;
+    default: 
+        a=3;
+        console.log(a);
+}
